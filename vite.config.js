3a5866d3e83934e96be98edb37a4154015ace5aa
MariaPanchenko <marysa2502@gmail.com>
2024-11-06 19:03:02 +0200

    <base href="/goit-react-hw-01/">

--- vite.config.js
+++ vite.config.js
@@ -2,10 +2,10 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
-export default defineConfig(({ mode }) => ({
+export default defineConfig({
   plugins: [react()],
   build: {
     sourcemap: true,
-    base: mode === 'production' ? '/goit-react-hw-01/' : '/',
+    base: '/goit-react-hw-01/',
   },
-}));
+});
