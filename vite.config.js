435ccdf5ab6a0989cbfca8ba8b2d416cee285989
MariaPanchenko <marysa2502@gmail.com>
2024-11-06 18:54:23 +0200

    vite build settings

--- vite.config.js
+++ vite.config.js
@@ -2,10 +2,10 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
-export default defineConfig({
+export default defineConfig(({ mode }) => ({
   plugins: [react()],
   build: {
     sourcemap: true,
-    base: /goit-react-hw-01/,
+    base: mode === 'production' ? '/goit-react-hw-01/' : '/',
   },
-});
+}));
